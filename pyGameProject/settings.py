7224acec7fefa1c8a31b71529671702716f86684
Aysu Avcı <aysuav9@gmail.com>
2025-04-09 17:31:23 +0300

    Alien İnvasion Ödev
    Alien invasion oyunu ödev bullet ve ship classı

---
+++ pyGameProject/settings.py
@@ -0,0 +1,12 @@
+class Settings:
+    def __init__(self):
+        # Ekran Ayarları
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (0,0,53)
+
+        # Mermi Ayarları
+        self.bullet_speed = 1.5
+
+        # Gemi Ayarları
+        self.ship_speed = 1.8
